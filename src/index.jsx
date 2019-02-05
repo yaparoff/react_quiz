d07ca5ad0cd5183ec8ef7392846b07e5674b255c
yaparoff <mr.yaparoff@yandex.ru>
2019-02-05 18:33:52 +0400

    add classNames; change structure

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './node_modules/containers/app/App';
+import { App } from './containers/app/App';
 import './normalize.css';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
